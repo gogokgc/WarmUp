a810844b89254a820667c71d544413fe40ec1a74
gogokgc <gogokgc@gmail.com>
2024-01-17 14:13:07 +0900

    Swift UI practice

---
+++ WarmUp/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  WarmUp
+//
+//  Created by KYUCHEOL KIM on 1/17/24.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Onboarding()
+}
