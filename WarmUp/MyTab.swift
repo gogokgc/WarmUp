b1d5f35207bba918775b60b954bb86bb04b557f8
gogokgc <gogokgc@gmail.com>
2024-02-02 13:52:46 +0900

    TabView Practice 2

--- WarmUp/MyTab.swift
+++ WarmUp/MyTab.swift
@@ -10,17 +10,27 @@ import SwiftUI
 struct MyTab: View {
     var body: some View {
         TabView {
-            Text("Tab Content 1")
+//            Text("Tab Content 1")
+            TabDetail()
                 .badge("!")
                 .tabItem {
                     Label("Label1", systemImage: "gear")
                 }.tag(1)
-            Text("Tab Content 2")
+//            Text("Tab Content 2")
+            Onboarding()
                 .badge(2)
                 .tabItem {
                     Label("Label2", systemImage: "gear.circle")
                 }.tag(2)
+            ListLoop()
+                .tabItem {
+                    Label("Label3", systemImage: "pencil.circle")
+                }.tag(3)
         }
+//        .tabViewStyle(.page)
+//        .tabViewStyle(.automatic)
+        .tabViewStyle(.page(indexDisplayMode: .always))
+        .background(.gray)
     }
 }
 
---
+++ WarmUp/TabDetail.swift
@@ -0,0 +1,18 @@
+//
+//  TabDetail.swift
+//  WarmUp
+//
+//  Created by KYUCHEOL KIM on 2/2/24.
+//
+
+import SwiftUI
+
+struct TabDetail: View {
+    var body: some View {
+        Text("Label1 Tab Deatail Page")
+    }
+}
+
+#Preview {
+    TabDetail()
+}
